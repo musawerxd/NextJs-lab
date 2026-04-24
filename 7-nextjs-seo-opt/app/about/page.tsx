bbe736d2e4e81d30664d7f913446ed49137103a3
musawerxd <hunnyshah0012@gmail.com>
2026-04-24 21:20:24 +0500

    Learn Next.js metadata object with multiple layouts

---
+++ 7-nextjs-seo-opt/app/about/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function page() {
+    return (
+        <div>
+            <h1 className="text-center font-bold">This is About Page</h1>
+        </div>
+    )
+}
+
+export default page
